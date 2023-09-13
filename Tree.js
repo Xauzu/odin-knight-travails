f85f51b3e9c41871de1f0f708ab7c2bd87a3063d
Xau <frozzy.xfs@gmail.com>
2023-09-12 21:52:24 -0400

    Add set moves, helper functions to validate moves

--- Tree.js
+++ Tree.js
@@ -4,10 +4,29 @@ module.exports = class Tree {
     constructor(position, moves) {
         this._pos = position;
         this._moves = moves;
+        if (this._moves)
+            this.#validateMoves();
     }
 
     get pos() { return this._pos; }
 
     get moves() { return this._moves; }
-    set moves(moves) { this._moves = moves; }
+    set moves(moves) { 
+        this._moves = moves;
+        this.#validateMoves();
+     }
+
+    validRange(coordinatesPair) {
+        return (coordinatesPair[0] >= 0 && coordinatesPair[0] < 8) && (coordinatesPair[1] >= 0 && coordinatesPair[1] < 8);
+    }
+
+    #validateMoves() {
+        let validMoves = [];
+        for (let i = 0; i < this._moves.length; i++) {
+            const move = this._moves[i];
+            if (this.validRange(move))
+                validMoves.push([move[0], move[1]]);
+        }
+        console.log(validMoves);
+    }
 }
