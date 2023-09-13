e0cbe75803333249277d31a379454fa0f58fbdc0
Xau <frozzy.xfs@gmail.com>
2023-09-12 23:21:56 -0400

    Moved validation functions, add knightMoves and trimSteps function

--- Tree.js
+++ Tree.js
@@ -1,32 +1,18 @@
 // position is an array of coordinate pair [x,y]
 // moves is an array of coordinate pair arrays
 module.exports = class Tree {
-    constructor(position, moves) {
+    constructor(position, moves, total) {
         this._pos = position;
-        this._moves = moves;
-        if (this._moves)
-            this.#validateMoves();
+        this._moves = moves || null;
+        this._totalMoves = total || 0;
     }
 
     get pos() { return this._pos; }
 
+    get totalMoves() { return this._totalMoves; }
+
     get moves() { return this._moves; }
     set moves(moves) { 
         this._moves = moves;
-        this.#validateMoves();
-     }
-
-    validRange(coordinatesPair) {
-        return (coordinatesPair[0] >= 0 && coordinatesPair[0] < 8) && (coordinatesPair[1] >= 0 && coordinatesPair[1] < 8);
-    }
-
-    #validateMoves() {
-        let validMoves = [];
-        for (let i = 0; i < this._moves.length; i++) {
-            const move = this._moves[i];
-            if (this.validRange(move))
-                validMoves.push([move[0], move[1]]);
-        }
-        console.log(validMoves);
     }
 }
--- main.js
+++ main.js
@@ -1,5 +1,92 @@
 const Tree = require('./Tree.js');
 const Board = require('./Board.js');
+const Queue = require('./Queue.js');
 
-const board = new Board(9);
-board.printGrid();
+const knightDisplacements = [
+    [1,2],
+    [2,1],
+    [2,-1],
+    [1,-2],
+    [-1,-2],
+    [-2,-1],
+    [-2,1],
+    [-1,2]
+];
+
+function validRange(coordinatesPair) {
+    return (coordinatesPair[0] >= 0 && coordinatesPair[0] < 8) && (coordinatesPair[1] >= 0 && coordinatesPair[1] < 8);
+}
+
+function validateMoves(steps) {
+    let validMoves = [];
+    for (let i = 0; i < steps.length; i++) {
+        const move = steps[i];
+        if (validRange(move))
+            validMoves.push([move[0], move[1]]);
+    }
+    return validMoves;
+}
+
+function trimSteps(board, x, y) {
+    let knightSteps = knightDisplacements.map((disp, index) => {
+        return [x + disp[0], y + disp[1]];
+    });
+
+    knightSteps = validateMoves(knightSteps);
+
+    const results = []
+    for (let i = 0; i < knightSteps.length; i++) {
+        if (board.getValue(x, y) === '') {
+            results.push(knightSteps[i]);
+        }
+    }
+    
+    return results;
+}
+
+// Way overboard for what was needed
+function knightMoves(start, end) {
+    const board = new Board('');
+
+    const startSteps = knightDisplacements.map((disp, index) => {
+        return [start[0] + disp[0], start[1] + disp[1]];
+    });
+
+    const root = new Tree(start, startSteps);
+
+    const q = new Queue;
+    q.enqueue(root);
+
+    while (q.length > 0) {
+        const item = q.dequeue();
+        [x, y] = item.pos;
+
+        // If values exist, it is already calculated
+        if (board.getValue(x, y) === '') {
+            board.setValue(x, y, item.totalMoves);
+
+            const newMoves = [];
+            for (let i = 0; i < item.moves.length; i++) {
+                // Check if position already has value
+                [branchX, branchY] = item.moves[i];
+                const knightSteps = trimSteps(board, branchX, branchY);
+
+                const branch = new Tree([branchX, branchY], knightSteps, item.totalMoves + 1);
+                newMoves.push(branch);
+                q.enqueue(branch);
+            }
+
+            item.moves = newMoves;
+        }
+    }
+
+    const stepsNeeded = board.getValue(...end);
+    console.log(`Steps required for (${end[0]}, ${end[1]}):`, stepsNeeded);
+
+    board.printGrid();
+
+    let moves = '';
+    return moves;
+}
+
+knightMoves([3,3], [7,7]);
